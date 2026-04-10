02130f302654fb43787190d6dd2d1fc980e8636d
Gagan Namdev <gagancomputer98@gmail.com>
2026-04-10 16:06:05 +0530

    Implemented user input logic to read number using nextInt()

--- GaganNamdev_java_training/Session_01/Control_Flow_Statements/MultiplicationTable.java
+++ GaganNamdev_java_training/Session_01/Control_Flow_Statements/MultiplicationTable.java
@@ -1,4 +1,15 @@
 import java.util.Scanner;
 public class MultiplicationTable {
+
+    public static void main(String[] args) {
+
+        // Scanner for user input
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input number
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        System.out.println("\nMultiplication Table of " + number + ":\n");
     
 }
