ce24056e139a91385216bcbdc5bc66859ac40598
Gagan Namdev <gagancomputer98@gmail.com>
2026-04-10 16:43:49 +0530

    Implemented logic to take array size and elements as input from user

--- GaganNamdev_java_training/Session_01/Arrays/LinearSearch.java
+++ GaganNamdev_java_training/Session_01/Arrays/LinearSearch.java
@@ -5,5 +5,16 @@ public class LinearSearch {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        //  array size input
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        //  array elements input
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
     
 }
