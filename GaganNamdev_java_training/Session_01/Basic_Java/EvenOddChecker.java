0abd9e96b7d72df405038ebd39a0544726b714b3
Gagan Namdev <gagancomputer98@gmail.com>
2026-04-10 14:43:13 +0530

    Implemented logic to check even or odd number

--- GaganNamdev_java_training/Session_01/Basic_Java/EvenOddChecker.java
+++ GaganNamdev_java_training/Session_01/Basic_Java/EvenOddChecker.java
@@ -14,3 +14,23 @@ public class EvenOddChecker {
 
         scanner.close();
     }
+
+    /**
+     * Method to take user input
+     */
+    private static int getInput(Scanner scanner) {
+        System.out.print("Enter a number: ");
+        return scanner.nextInt();
+    }
+
+    
+     // Method to check even or odd
+    
+    private static void checkEvenOdd(int number) {
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+    }
+}
