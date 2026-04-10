504a664e531fa35fe8c3299741e8ec9728bb4861
Gagan Namdev <gagancomputer98@gmail.com>
2026-04-10 14:58:58 +0530

    Implemented Fibonacci series logic using loop

--- GaganNamdev_java_training/Session_01/Basic_Java/FibonacciSeries.java
+++ GaganNamdev_java_training/Session_01/Basic_Java/FibonacciSeries.java
@@ -1,3 +1,34 @@
+import java.util.Scanner;
+
 public class FibonacciSeries {
-    
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter limit: ");
+        int limit = sc.nextInt();
+
+        if (limit <= 0) {
+            System.out.println("Please enter a positive number");
+            return;
+        }
+
+        int firstNumber = 0;
+        int secondNumber = 1;
+
+        System.out.println("Fibonacci Series:");
+
+        for (int i = 1; i <= limit; i++) {
+
+            System.out.print(firstNumber + " ");
+
+            int nextNumber = firstNumber + secondNumber;
+
+            firstNumber = secondNumber;
+            secondNumber = nextNumber;
+        }
+
+        sc.close();
+    }
 }
