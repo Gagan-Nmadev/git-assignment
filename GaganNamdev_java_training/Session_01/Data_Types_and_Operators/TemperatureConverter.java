4dc3fc715dd1f0acc112c9b3849a25b5a5b6748e
Gagan Namdev <gagancomputer98@gmail.com>
2026-04-10 15:39:43 +0530

    Fahrenheit to Celsius conversion

--- GaganNamdev_java_training/Session_01/Data_Types_and_Operators/TemperatureConverter.java
+++ GaganNamdev_java_training/Session_01/Data_Types_and_Operators/TemperatureConverter.java
@@ -1,5 +1,5 @@
 public class TemperatureConverter {
-    
+
     //  Celsius to Fahrenheit conversion
     public void celsiusToFahrenheit(double celsius) {
 
@@ -7,5 +7,14 @@ public class TemperatureConverter {
 
         System.out.println("Celsius to Fahrenheit:");
         System.out.println(celsius + " °C = " + fahrenheit + " °F");
+    }
+
+    // Fahrenheit to Celsius conversion
+    public void fahrenheitToCelsius(double fahrenheit) {
+
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+        System.out.println("\nFahrenheit to Celsius:");
+        System.out.println(fahrenheit + " °F = " + celsius + " °C");
     }
 }
