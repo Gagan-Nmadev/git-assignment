da776f85978e3bf529574219925d6cdae9f9fec3
Gagan Namdev <gagancomputer98@gmail.com>
2026-04-19 23:42:06 +0530

    Created TodoRepository using JpaRepository

--- GaganNamdev_java_training/Session_01/java/session4/todoapp/todoapp/src/main/java/com/training/todoapp/repository/TodoRepository.java
+++ GaganNamdev_java_training/Session_01/java/session4/todoapp/todoapp/src/main/java/com/training/todoapp/repository/TodoRepository.java
@@ -1,4 +1,10 @@
 package com.training.todoapp.repository;
 
-public class TodoRepository {
+import com.training.todoapp.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+    // Provides built-in CRUD operations for Todo entity
 }
